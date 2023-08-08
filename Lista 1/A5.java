26e43fe48ea0e201055a5aa5a75bb71201de3ad7
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-08-08 17:22:01 -0300

    Lista finalizada

--- Lista 1/A5.java
+++ Lista 1/A5.java
@@ -1,51 +1,59 @@
 /*
- * Implemente o quicksort em uma função para ordernar uma lista de números
+ * Implemente o QuickSort em uma função para ordernar uma lista de números
  * recebida como parâmetro. Após a ordenação, a função deve retornar a lista
  * ordenada.
 */
 
+import java.util.Scanner;
+
 public class A5 {
-    
-    class Wrapper {
-        
-        public int value;
-        
-        public Wrapper(int value) {
-            this.value = value;
-        }
-    }
 
-    public static void particao(int esq, int dir, Wrapper i, Wrapper j, int[] list) {
-        int x, w;
-        i.value = esq; j.value = dir;
-        x = list[(i.value + j.value)/2];
-
-        do {
-            while(x > list[i.value]) i.value++;
-            while(x < list[j.value]) j.value--;
-            if(i.value <= j.value) {
-                w = list[i.value];
-                list[i.value] = list[j.value];
-                list[j.value] = w;
-                i.value++; 
-                j.value--;
+    public static int separar(int[] list, int ini, int fim) {
+        int pivo = list[ini];
+        int i = ini + 1, f = fim;
+        
+        while (i <= f) {
+            if      (list[i] <= pivo) i++;
+            else if (list[f] > pivo)  f--;
+            else {
+                int troca = list[i];
+                list[i] = list[f];
+                list[f] = troca;
+                i++;
+                f--;
             }
-        } while(i.value <= j.value);
+        }
+
+        list[ini] = list[f];
+        list[f] = pivo;
+        return f;
     }
 
-    public static void ordena(int esq, int dir, int[] list) {
-        Wrapper i = new Wrapper(2); i.value = esq;   
-        Wrapper j = new Wrapper(2); j.value = dir;
-        
-        particao(esq, dir, i, j, list);
-        if(esq < j.value) ordena(esq, j.value, list);
-        if(i.value < dir) ordena(i.value, dir, list);
-    }
-    public static void main(String[] args) {
-        int[] list = {5,4,3,2,1};
-        ordena(0, 4, list);
-        for (int x : list) {
-            System.out.print(x);
+    public static void sort(int[] list, int ini, int fim) {
+        if (ini < fim) {
+            int pivot = separar(list, ini, fim);
+            sort(list, ini, pivot - 1);
+            sort(list, pivot + 1, fim);
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantidade de elementos: ");
+
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+
+        System.out.print("Lista original: ");
+
+        for(int i=0; i<size; i++) 
+            list[i] = scanner.nextInt();
+
+        sort(list,0,size-1);
+
+        System.out.print("Lista ordenada: ");
+        for (int i : list) System.out.print(i + " "); System.out.println();
     }
 }
